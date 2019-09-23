5b643d5a541c55d203801eb0974b13bd9943f56c
ingenworks1 <ingenworks1@DESKTOP-78UCUQ1>
2019-09-23 13:08:05 +0530

    first commit

---
+++ STSGithubdemoProject/src/main/java/com/ingen/gitdemo/STSGithubdemoProject/App.java
@@ -0,0 +1,13 @@
+package com.ingen.gitdemo.STSGithubdemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
